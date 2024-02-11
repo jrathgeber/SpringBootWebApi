7d9e908f9b2e067986b93adfa8743f67b07777a0
jrathgeber <jrathgeber@yahoo.com>
2024-02-11 17:43:59 -0500

    secret

--- src/main/java/com/mechanizd/springbootwebapi/SpringbootwebapiApplication.java
+++ src/main/java/com/mechanizd/springbootwebapi/SpringbootwebapiApplication.java
@@ -3,6 +3,7 @@ package com.mechanizd.springbootwebapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -15,6 +16,15 @@ public class SpringbootwebapiApplication {
 		return "Congrats !!! your app deployed successfully in Azure ! ";
 
 	}
+
+
+	@GetMapping("/secret")
+	public String secret() {
+
+		return "This is a secret message ";
+
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootwebapiApplication.class, args);
